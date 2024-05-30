a7a71935c98608bcb1cb17756be268f9afc46a94
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-30 15:34:01 +0700

    membuat int main queues

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -108,9 +108,19 @@ public :
         FRONT_position = 0;
 
         // Iterasi dari awal array hingga REAR
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;
     }
-
 
    }
 
 };
+
+int main()
+{
+    Queues q;
+}
