cf1c5c55269cbf85c45fd9547c4398851f330511
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-30 15:31:08 +0700

    iterasi dari awal array hingga REAR

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -91,7 +91,23 @@ public :
         while (FRONT_position <= REAR_position)
         {
             cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
         }
+        cout << endl;
+    }
+
+    else
+    {
+        //jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+        while (FRONT_position <= max - 1)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+
+        FRONT_position = 0;
+
+        // Iterasi dari awal array hingga REAR
     }
 
 
