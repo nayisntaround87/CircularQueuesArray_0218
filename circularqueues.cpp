2639b866a88661e4d478d73cae8aa47c29655fde
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-30 14:39:01 +0700

    membuat void insert

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 class queues 
 {
-    private :
+private :
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public :
+    queues() 
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "enter a number: ";
+        cin >> num;
+    }
 };
